088200fc74e4fcae09aedee1ad1138f531df8592
Austin Lee <austinlee@usf.edu>
2025-09-06 23:12:35 -0400

    debug

--- color.js
+++ color.js
@@ -1,7 +1,7 @@
 const body=document.getElementsByTagName("body") [0]
-function setColor(name) {body.style.backgroundColor=name}
+function setColor(name) {body.style.backgroundColor=name;}
 
-function randomColor(){\
+function randomColor(){
     const red=Math.round (Math.random()*255)
     const green=Math.round (Math.random()*255)
     const blue=Math.round (Math.random()*255)
