387a6e75ef7516b905f905d3c9c3726e6517c1e5
Austin Lee <austinlee@usf.edu>
2025-09-06 23:22:29 -0400

    revert to og

--- color.js
+++ color.js
@@ -1,3 +1,13 @@
 const body=document.getElementsByTagName("body") [0]
 function setColor(name) 
 {body.style.backgroundColor=name;}
+
+function randomColor(){
+    const red=Math.round (Math.random()*255)
+    const green=Math.round (Math.random()*255)
+    const blue=Math.round (Math.random()*255)
+    const yellow=Math.round (Math.random()*255)
+
+    const color=`rgby(${red}, ${green}, ${blue}, ${yellow})`
+    body.style.backgroundColor=color;
+}
